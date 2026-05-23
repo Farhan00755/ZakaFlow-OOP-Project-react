cbb11b237cb66d1c4351f42010b8a3bd67104678
Farhan00755 <tyoprasetyo2007@gmail.com>
2026-05-23 17:49:41 +0700

    memperbaiki tampilan website

--- src/main/java/com/zakaflow/zakaflow/config/SecurityConfig.java
+++ src/main/java/com/zakaflow/zakaflow/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/programs/**", "/categories/**", "/login", "/register", "/error", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/", "/programs/**", "/categories/**", "/login", "/register", "/error", "/css/**", "/js/**", "/img/**").permitAll()
                         .requestMatchers("/transactions/**").hasAnyRole("ADMIN", "DONATUR")
                         .anyRequest().hasRole("ADMIN"))
                 .formLogin(form -> form
